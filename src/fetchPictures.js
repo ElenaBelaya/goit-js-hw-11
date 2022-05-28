fa3142fa04a6c0831657c5b1554533bd5d8a698e
ElenaBelaya <makseniya@ukr.net>
2022-05-28 19:59:49 +0300

    part 2
    I need axios more

--- src/fetchPictures.js
+++ src/fetchPictures.js
@@ -1,11 +1,30 @@
-//export const BASE_URL = 'https://restcountries.com/v3.1';
-export function fetchPictures(picturesName) {
-    return  fetch(`https://pixabay.com/api/?key=27675363-36ea1c192b15f6a71011fab08&q=${picturesName}s&image_type=photo&orientation=horizontal&safesearch=true`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(response.status);
-          }
-        return response.json();
-     });
-    
- };
+export default class NewFetchPictures {
+constructor() {
+  this.picturesName = '';
+  this.page = 1;
+}
+ 
+fetchPictures() {
+ 
+    return fetch(`https://pixabay.com/api/?key=27675363-36ea1c192b15f6a71011fab08&q=${this.picturesName}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`)
+    .then(response => response.json())
+    .then((data) => {
+     this.page +=1;
+     return data;
+                
+ });
+}
+
+ get searchQuery() {
+     return this.picturesName;
+ }
+
+ set searchQuery(newsearchQuery) {
+   this.picturesName = newsearchQuery;
+ }
+
+ resetPage() {
+   this.page = 1;
+ }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,39 +1,54 @@
+import SimpleLightbox from "simplelightbox";
 import './css/styles.css';
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import {fetchPictures} from "./fetchPictures";
+import NewFetchPictures from "./fetchPictures";
+
 
 const refs = {
     formEl: document.querySelector(`#search-form`),
     inputEl: document.querySelector(`input`),
     btn: document.querySelector(`button[type="submit"]`),
     galleryBox:document.querySelector(`.gallery`),
-
+    btnAddMore: document.querySelector(`button[type="button"]`),
 }
 
+const newFetchPictures = new NewFetchPictures();
+
 refs.formEl.addEventListener(`submit`, onSearch);
+refs.galleryBox.addEventListener(`click`, onViewingStart);
+refs.btnAddMore.addEventListener(`click`, onAddMore);
 
 function onSearch(event) {
 event.preventDefault();
 refs.galleryBox.innerHTML = "";
-const picturesName = refs.inputEl.value.trim();
-    if(picturesName) {
-       fetchPictures(picturesName)
-        .then(renderPictures)
-        .catch(error => console.log(error.message));
-    }
-       
+newFetchPictures.searchQuery = event.currentTarget.elements.searchQuery.value;
+newFetchPictures.resetPage();
+if(newFetchPictures.searchQuery) {
+newFetchPictures.fetchPictures().then(renderPictures)
+   .catch(error => console.log(error.message));
+   refs.btnAddMore.classList.add(`active`);
+   }  
+
 };
 
+function onAddMore() {  
+  newFetchPictures.fetchPictures().then(renderPictures)
+  .catch(error => console.log(error.message));
+};
+  
+
 function renderPictures(pictures) {
-  console.log(pictures);
+console.log(pictures);
   if(pictures.hits.length === 0) {
+    refs.btnAddMore.classList.remove(`active`);
     Notify.failure("Sorry, there are no images matching your search query. Please try again.");
   }  else {
-    Notify.success("Hooray! We found totalHits images.");
+    Notify.success(`Hooray! We found totalHits ${pictures.total} images.`);
     const pictureList = pictures.hits.map((picture) => 
-    `
-    <div class="photo-card">
-<img src="${picture.webformatURL}" alt="${picture.tags}" loading="lazy" />
+     `     
+    <a class="photo-card" href="${picture.largeImageURL}">
+    <img src=${picture.webformatURL}" alt="${picture.tags}" title="${picture.tags}" loading="lazy" />      
 <div class="info">
  <p class="info-item">
    <b>Likes</b>${picture.likes}
@@ -48,7 +63,8 @@ function renderPictures(pictures) {
    <b>Downloads</b>${picture.downloads}
  </p>
 </div>
-</div>
+</a>  
+
 `
 ).join("");
 
@@ -57,8 +73,19 @@ refs.galleryBox.insertAdjacentHTML("beforeend", pictureList);
 } 
   };
   
+  let gallery = new SimpleLightbox('.gallery a',{captionsData: "alt"}); 
   
+
+function onViewingStart(event) {  
   
+  gallery.on('show.simplelightbox'); 
+  //console.log(`hello`); 
+  gallery.refresh(); 
+};
+
+
+
+ 
 
 
 
